e5f3ad6f7e56956f9114443322e26b8ddb6094e5
Emmanuel Esposito <14996617+emmanuelespo@users.noreply.github.com>
2018-09-07 01:49:05 +0200

    Created script for using lexer only, improved syntax error message

--- src/main/java/lt/compiler/SyntaxErrorException.java
+++ src/main/java/lt/compiler/SyntaxErrorException.java
@@ -10,12 +10,35 @@ public class SyntaxErrorException extends RuntimeException {
 	private int column;
 	private List<String> expectedTokens;
 
+	public static String createMessage(Symbol token, int line, int column, List<String> expectedTokens) {
+		StringBuilder sb = new StringBuilder();
+		sb.append("Syntax error at line ");
+		sb.append(line);
+		sb.append(", column ");
+		sb.append(column);
+		sb.append(": unexpected ");
+		if (token.sym != ParserSym.error) {
+			sb.append(token.toString().substring(8));
+		} else {
+			sb.append('"');
+			sb.append(token.value);
+			sb.append('"');
+		}
+		sb.append(" token");
+		return sb.toString();
+	}
+
 	public SyntaxErrorException(Symbol token, int line, int column, List<String> expectedTokens) {
-		super("Syntax error at line " + line + ", column " + column + ": unexpected " + token.toString().substring(8) + " token");
+		super(createMessage(token, line, column, expectedTokens));
+
 		this.token = token;
 		this.line = line;
 		this.column = column;
 		this.expectedTokens = expectedTokens;
+	}
+
+	public Symbol getToken() {
+		return token;
 	}
 
 	public int getLine() {
