d7518fc52ef4a66c7c70a9874f11e14174e109ca
Emmanuel Esposito <14996617+emmanuelespo@users.noreply.github.com>
2018-09-07 10:30:31 +0200

    Reorganised examples with generated stack machine code

---
+++ compileExamples.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+for t in $(ls examples); do
+	echo Compiling examples/$t/$t.mylang
+	./compile.sh examples/$t/$t.mylang examples/$t/$t.o && \
+	./exec.sh -L examples/$t/$t.o > examples/$t/$t.out
+	[ -f examples/$t/$t.o ] || rm -fv examples/$t/$t.out
+done
